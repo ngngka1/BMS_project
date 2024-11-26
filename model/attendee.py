90e3e705b7dd5424fb9aa822c40655ee7887ddc5
JoJo <hello.jerrym@gmail.com>
2024-11-26 20:42:45 +0800

    sql

--- model/attendee.py
+++ model/attendee.py
@@ -15,9 +15,8 @@ class Attendee:
                 Type CHAR(7) NOT NULL CHECK(Type IN ("Staff", "Student", "Alumni", "Guest")),
                 Phone_No CHAR(8) NOT NULL CHECK(LENGTH(Phone_No) = 8 AND Phone_No NOT LIKE '% '),
                 Address VARCHAR(255) NOT NULL,
-                Organization VARCHAR(40) NOT NULL,
-                PRIMARY KEY(Email_Address)
-                
+                Organization CHAR(6) NOT NULL CHECK("PolyU", "SPEED", "HKCC", "Others"),
+                PRIMARY KEY(Email_Address)    
             )
         ''')
         Attendee.__db_connection.commit()
--- model/banquet.py
+++ model/banquet.py
@@ -1,27 +1,28 @@
 import sqlite3
-class Staff:
+class Banquet:
     __db_connection = None
     
     def __init__(self, db_connection: sqlite3.Connection):
-        # if not Staff.__db_connection is None:
+        # if not Banquet.__db_connection is None:
         #     raise Exception('Database connection already exists')
-        Staff.__db_connection = db_connection
-        Staff.__db_connection.execute('''
-            CREATE TABLE Staff (
-                Staff_No CHAR(8) NOT NULL,
-                First_Name VARCHAR(20) NOT NULL,
-                Last_Name VARCHAR(20) NOT NULL,
-                Department CHAR(22) NOT NULL CHECK(Department IN ("Catering", "Events Services", "Kitchen Staff", "Bar Services", "Facilities Management", "Audio and Visual", "Decor and Design", "Guest Services", "Security")),            
-                PRIMARY KEY(Staff_No)
-                
+        Banquet.__db_connection = db_connection
+        Banquet.__db_connection.execute('''
+            CREATE TABLE Banquet (
+              BIN INT NOT NULL CHECK(BIN > 0),
+              Banquet_Name VARCHAR(20) NOT NULL CHECK(Banquet_Name ~ '^[A-Za-z]+$');
+              Available BOOLEAN NOT NULL,
+              Quota INT NOT NULL CHECK(Quota > 0),
+              Address VARCHAR(255) NOT NULL,
+              Location VARCHAR(6) NOT NULL,
+              PRIMARY KEY(BIN)
             )
         ''')
-        Staff.__db_connection.commit()
+        Banquet.__db_connection.commit()
     
     def insert(*args):
-        Staff.__db_connection.execute(f'''
-            INSERT INTO Staff VALUES ({args})
+        Banquet.__db_connection.execute(f'''
+            INSERT INTO Banquet VALUES ({args})
         ''')
-        Staff.__db_connection.commit()
+        Banquet.__db_connection.commit()
         
         
