b49b91896e805d3fa2d0d686999ce4554b89b775
ngngka1 <ngwewe2015@gmail.com>
2024-11-30 22:18:53 +0800

    final

--- view/staff.py
+++ view/staff.py
@@ -10,7 +10,7 @@ class StaffView(BaseView):
             "Staff list": "list all the staff",
             "Staff attend <staff_no> <bin>": "tick attendance of the staff on the banquet with bin",
             "Staff create <first_name> <last_name> <department>": "create a Staff record",
-            "Staff update <first_name> <last_name> <department>": "update a Staff record"
+            "Staff update <staff_no> <first_name> <last_name> <department>": "update a Staff record"
         })
         self.add_argument_prompt({
             "staff_no": "Input the staff no. of the contact staff",
